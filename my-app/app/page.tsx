3ee93c7b43495400d6074f0aa1708197fb509ced
diogoMesquitaDeepSkill <diogomesquita@deepskill.com>
2025-01-06 22:54:21 +0100

    Project installation, ImagesSlider, Hero, Footer

---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+import { ImagesSliderDemo } from "@/components/ui/demo";
+import Footer from "@/components/ui/footer";
+import Hero from "@/components/ui/hero";
+
+export default function Home() {
+  return (
+    <div>
+      <ImagesSliderDemo />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
